7b7ced409dd18c1dc42b4a5d434df333a9b88579
Sameerverma32 <sameerverma8318@gmail.com>
2025-02-26 16:51:33 +0530

    main

--- Dashboard/pages/deshboard.py
+++ Dashboard/pages/deshboard.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 df = sns.load_dataset('titanic')
 
-st.title('Titanic deshboard') 
-st.markdown("This is a simple dashboard for Titanic dataset where you can explore the dataset and visualize it ")
+st.title('Titanic Dashboard')
+st.markdown("This is a simple dashboard for Titanic dataset where you can explore the dataset and visualize it") 
+
 st.dataframe(df) # Display the dataset
 
 st.sidebar.header("Filter options")
@@ -17,29 +18,30 @@ gender = st.sidebar.multiselect('Gender',
                                 default=df['sex'].unique())
 
 #class filter
-pclass=st.sidebar._multiselect('class',
-                               options=sorted(df["class"].unique()),
-                               default=sorted(df['class'].unique()))
+pclass = st.sidebar.multiselect('Class',
+                                options = sorted(df['class'].unique()),
+                                default = sorted(df['class'].unique()))
 
 #age filter
-min_age, max_age = st.sidebar.slider('age',
-                                     min_value = int(df['age'].min()),
-                                     max_value = int(df['age'].max()),
-                                     value = (int(df['age'].min()), int(df['age'].max())))
+min_age, max_age = st.sidebar.slider('Age',
+                                    min_value = int(df['age'].min()),
+                                    max_value = int(df['age'].max()),
+                                    value = (int(df['age'].min()), int(df['age'].max())))
+
+filtered_data = df[
+    (df['sex'].isin(gender)) &
+    (df['class'].isin(pclass)) &
+    (df['age'] >= min_age) &
+    (df['age'] <= max_age)
+]
 
 #create a pie chart for gender distribution
 st.subheader("Gender Distribution")
-fig = px.pie(df, names = 'age', title="Gender Distribution")
+gender_count = filtered_data['sex'].value_counts()
+fig = px.pie(names=gender_count.index, values=gender_count.values,
+     title="Gender Distribution")
 st.plotly_chart(fig)
 
-#create a histogram for age distrinution
-fig = px.histogram(df, x='age', title="Age Distribution", nbins=20)
-st.plotly_chart(fig)
-
-#create a bar plot for survival rate by class
-fig = px.scatter(df, x='class', y='survived', title="Survival rate by Class")
-st.plotly_chart(fig)
-
-#bar plot
-fig = px.line(df, x='age', y='fare', title="Age & Fare")
+#create a histogramfor age distribution
+fig = px.histogram(filtered_data, x='age', title="Age Distribution", nbins=20)
 st.plotly_chart(fig)
